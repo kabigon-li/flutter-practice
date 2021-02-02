09362391313b133a145a90705e969f272433f3e9
李佳丽 <lijiali0515@yahoo.co.jp>
2021-02-02 23:46:08 +0900

    Add:mypage_update

--- lib/tab/mypage.dart
+++ lib/tab/mypage.dart
@@ -11,7 +11,42 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('mypage'),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.green[200],
+          leading: Icon(
+            Icons.account_circle,
+            color: Colors.black,
+          ),
+          title: Center(
+            child: Text(
+              'Setting',
+              style: TextStyle(fontFamily: 'Cursive', fontSize: 30),
+            ),
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.people),
+                title: Text("Dark mode"),
+                trailing: Icon(Icons.more_vert),
+                
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.people),
+                title: Text("font size"),
+                trailing: Icon(Icons.more_vert),
+                
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
