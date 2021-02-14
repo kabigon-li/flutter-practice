e01660c28edaee16262f3263343c48fcbffa4b94
李佳丽 <lijiali0515@yahoo.co.jp>
2021-02-14 14:09:42 +0900

    Add:user_provider

---
+++ lib/provider/user_provider.dart
@@ -0,0 +1,43 @@
+
+
+import 'package:flutter/foundation.dart';
+import 'package:wechat_like_memo/model/user.dart';
+
+class UserProvider with ChangeNotifier {
+  UserProvider({
+    this.userList,
+  });
+
+  List<User> userList;
+
+  // 追加、削除、更新、取得
+  // Create, Read, Update, Delete == CRUD
+
+  //クラス中の関数
+  void addUser(
+    User user, //受け取りたいやつ
+  ) {
+    userList.add(user);
+    notifyListeners();
+  }
+
+  void updateuser(
+    //2, うけとる
+    int id,
+    User newuser,
+  ) {
+    final userIndex = userList.indexWhere(
+      (user) => user.id == id,
+    );
+    userList[userIndex] = newuser;
+    notifyListeners();
+  }
+
+  void deleteuser(int id) {
+    final userIndex = userList.indexWhere(
+      (user) => user.id == id
+      );
+    userList.removeAt(userIndex);
+    notifyListeners();
+  }
+}
