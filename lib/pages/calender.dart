63d4741b42525315bedd2c5713047fb275a2c77a
李佳丽 <lijiali0515@yahoo.co.jp>
2021-04-22 22:35:58 +0900

    update: calender todo update

--- lib/pages/calender.dart
+++ lib/pages/calender.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
-
-  
+import 'package:provider/provider.dart';
+import 'package:wechat_like_memo/model/todo.dart';
+import 'package:wechat_like_memo/provider/database_provider.dart';
+import 'package:wechat_like_memo/provider/todo_provider.dart';
 
 class Calender extends StatefulWidget {
   @override
@@ -22,30 +24,243 @@ class _CalenderState extends State<Calender> {
 
   @override
   Widget build(BuildContext context) {
+    final todoProvider = Provider.of<TodoProvider>(context, listen: false);
     return new Scaffold(
-        appBar: AppBar(
-          title: Text("Calender Example"),
-        ),
-        body: Container(
-          child: CalendarCarousel<Event>(
-              onDayPressed: onDayPressed,
-              weekendTextStyle: TextStyle(color: Colors.red),
-              thisMonthDayBorderColor: Colors.grey,
-              weekFormat: false,
-              height: 420.0,
-              selectedDateTime: _currentDate,
-              daysHaveCircularBorder: false,
-              customGridViewPhysics: NeverScrollableScrollPhysics(),
-              markedDateShowIcon: true,
-              markedDateIconMaxShown: 2,
-              todayTextStyle: TextStyle(
-                color: Colors.blue,
+      appBar: AppBar(
+        title: Text("Calender Example"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+         
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'image/chatpageimage.jpg',
+                    
+                  ),
+                   fit: BoxFit.cover,
+                ),
               ),
-              markedDateIconBuilder: (event) {
-                return event.icon;
+          child: Column(
+            children: [
+              CalendarCarousel<Event>(
+                onDayPressed: onDayPressed,
+                weekendTextStyle: TextStyle(color: Colors.red),
+                thisMonthDayBorderColor: Colors.grey,
+                weekFormat: false,
+                height: 420.0,
+                selectedDateTime: _currentDate,
+                daysHaveCircularBorder: false,
+                customGridViewPhysics: NeverScrollableScrollPhysics(),
+                markedDateShowIcon: true,
+                markedDateIconMaxShown: 2,
+                todayTextStyle: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 12,
+                ),
+                markedDateIconBuilder: (event) {
+                  return event.icon;
+                },
+                todayBorderColor: Colors.green,
+                markedDateMoreShowTotal: false,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'image/chatpageimage.jpg',
+                    ),
+                  ),
+                ),
+                child: ListView.builder(
+                  //physics: const AlwaysScrollableScrollPhysics(),
+                  shrinkWrap: true, // 高さ関連のエラーが出たら、使う
+                  itemCount: todoProvider.todoList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return todo(
+                      // 1. Todo(id: 0, content: 'k', isChecked: 0)
+                      // 2. Todo(id: 1, content: 'kabigon', isChecked: 0)
+                      todoProvider.todoList[index], //调用todo这个方法时，这获取画面中更新的每行Todo
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget todo(
+    // Todo(id: 0, content: 'k', isChecked: 0)
+    Todo todoNew, //这接收新一个Todo
+  ) {
+    final todoProvider = Provider.of<TodoProvider>(context);
+    final databaseProvider = Provider.of<DataBaseProvider>(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //显示在屏幕上的每一条todo，由打沟框，todo内容，和删除按钮组成
+        Card(
+          color: Colors.grey[200],
+          child: CheckboxListTile(
+            //打勾框
+            activeColor: Colors.blue,
+            value: todoNew.isChecked == 0 ? false : true,
+            onChanged: (v) {
+              //クタスの実体化、Todoをtodoに代入
+              setState(() {
+                // checkboxl押された時,下実行する
+                Todo newTodo = Todo(
+                  //Todoの実体化（クラスのインスタンス）
+                  //受け取るやつ次第
+                  id: todoNew.id, //受け取るのTodoのid
+                  content: todoNew.content, //受け取るTodoの内容
+                  isChecked: todoNew.isChecked == 0 ? 1 : 0, //もし０、１になる、もし１、０になる
+                );
+                todoProvider.updateTodo(
+                  //1, 渡す 0
+                  todoNew.id,
+                  newTodo,
+                );
+
+                databaseProvider.updateTodo(
+                  newTodo,
+                );
+              });
+            },
+
+            //todo内容部分
+            title: SizedBox(
+              //height: 20,
+              //クリック編集
+              child: GestureDetector(
+                onTap: () {
+                  updateBottomSheet(
+                    //例えば、カビゴン書いたら、ここに渡す
+                    todoNew.id, //获取第一行的id，0
+                    todoNew.isChecked, //获取第一行的是否勾选
+                  );
+                },
+                child: Text(
+                  todoNew.content,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    decoration: todoNew.isChecked == 0
+                        ? null
+                        : TextDecoration.lineThrough,
+                  ),
+                ),
+              ),
+            ),
+
+            //删除按钮
+            secondary: GestureDetector(
+              child: Icon(Icons.delete),
+              onTap: () {
+                //todoNew.id
+                deleteTodoSheet(todoNew.id);
               },
-              todayBorderColor: Colors.green,
-              markedDateMoreShowTotal: false),
-        ));
+            ),
+          ),
+        ),
+
+        //分割线
+        const Divider(
+          height: 2,
+          thickness: 1,
+          color: Colors.grey,
+          indent: 10,
+          // endIndent: 20,
+        ),
+      ],
+    );
+  }
+
+  void deleteTodoSheet(int index) {
+    // TodoProviderクラスのインスタンス(コピー)を変数に代入
+    final todoProvider = Provider.of<TodoProvider>(context, listen: false);
+    //　databaseの実体化
+    final databaseProvider =
+        Provider.of<DataBaseProvider>(context, listen: false);
+    todoProvider.deleteTodo(
+      //1, 渡す 0
+      index,
+    );
+    databaseProvider.deleteTodo(index);
+  }
+
+//updateBottom接收
+  void updateBottomSheet(int index, int f) {
+    //index受け取る
+    // TodoProviderクラスのインスタンス(コピー)を変数に代入
+    final todoProvider = Provider.of<TodoProvider>(context, listen: false);
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 80,
+                //width: size.width,
+                child: Row(
+                  //Row Column中・二個か二個以上widgetの間隙間決める
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: ColoredBox(
+                        color: Colors.white,
+                        child: TextFormField(
+                          maxLines: 20,
+                          //initialValue: text,
+                          // onChanged: (String t) {
+                          //   chatbox(t);
+                          // },
+                          decoration: InputDecoration(
+                            hintText: 'to do',
+                            contentPadding: const EdgeInsets.all(10),
+                            // border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                      width: 60,
+                      child: RaisedButton(
+                        child: Text('編集'),
+                        color: Colors.blueGrey,
+                        onPressed: () {
+                          //クタスの実体化、Todoをtodoに代入
+                          Todo newTodo = Todo(
+                            id: index,
+                            //content: text,
+                            isChecked: f,
+                          );
+                          todoProvider.updateTodo(
+                            //1, 渡す 0
+                            index,
+                            newTodo,
+                          );
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
