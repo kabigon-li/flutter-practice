2c5b702665dc68bfdbec68f8d1b6cb827dba2d50
李佳丽 <lijiali0515@yahoo.co.jp>
2021-05-26 12:22:55 +0900

    update: fontsize provider add isFontSizeSelected

--- lib/provider/font_size_provider.dart
+++ lib/provider/font_size_provider.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 class FontSizeProvider extends ChangeNotifier {
   FontSizeProvider({
     this.fontSize = 15.0,
+    this.isFontSizeSelected = false,
   });
 
   double fontSize;
+  bool isFontSizeSelected;
+
+   void updateIsImageSelected(bool value) {
+    isFontSizeSelected = value;
+    notifyListeners();
+  }
 
   void updateFontSize(
     double newFontSize,
